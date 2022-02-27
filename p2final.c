feba67c9cfb190d8dd816d1c51b3eb3d2faf80d4
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-02-27 15:27:33 +0000

    p2 F

--- p2final.c
+++ p2final.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("enter the value of x1 and y1\n");
+  scanf("%f %f",x1,y1);
+  printf("enter the value of x2 and y2\n");
+  scanf("%f %f",x2,y2);
+  printf("enter the value of x3 and y3\n");
+  scanf("%f %f",x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
 
+}
