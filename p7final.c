317eec13b94bc106430c7810351751d11a2a9adb
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-03-17 06:48:29 +0000

    p7 F

--- p7final.c
+++ p7final.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+struct_point
+{
+  float x,y;
+};
+typedef struct_point Point;
+struct_line
+{
+  point p1,p2;
+ float distance;
+};
+typedef struct_line Line;
+Point input_point()
+{
+  point p1;
+  printf("enter the values of x and y\n");
+  scanf("%f %f",&p1.x,&p1.y);
+  return p2;
+}
+Line input_line()
+{
+  Line 1;
+  Point p1,p2;
+  p1=input_point();
+  p2=input_point();
+  1.p1=p1;
+  1.p2=p2;
+  return 1;
+}
 
